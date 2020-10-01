8ed7ef8393ad73b7085d22f080d4ed20df1827e5
Insoooooooooo <oasisholics@gmail.com>
2020-10-02 03:44:49 +0900

    add full post function

--- src/api/User/seeUser/seeUser.js
+++ src/api/User/seeUser/seeUser.js
@@ -1,9 +1,11 @@
 export default {
     Query:{
-        seeUser: (_, args, {request, isAuthenticated, prisma}) => {
+        seeUser: async(_, args, {request, isAuthenticated, prisma}) => {
             
             const {id} = args;
-            return prisma.user({id});
+            const user = await prisma.user({id});
+            const posts = await prisma.user({id}).posts();
+            return {user, posts};
         }
     }
 }
