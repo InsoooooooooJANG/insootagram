b81e4ba4a2e6f8e1299fc14f1760d2c88dac77ad
isjang <oasisholics@gmail.com>
2020-10-08 00:15:40 +0900

    Add Exception

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,6 +4,16 @@ export default {
 	Mutation: {
 		createAccount: async (_, args) => {
 			const { username, email, firstName="", lastName="", bio="" } = args;
+			const username_exists = await prisma.$exists.user({username});
+			if(username_exists){
+				throw Error("This username is alraedy exists");
+				return false;
+			}
+			const email_exists = await prisma.$exists.user({email});
+			if(email_exists){
+				throw Error("This email is already exists. Try other email");
+				return false;
+			}
 			const user = await prisma.createUser(
 				{
 					username, 
